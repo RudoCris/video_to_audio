96968fdb668c48e3326bb046d5a86063bae4f879
Rudolf <rud.manusachi@ru.ibm.com>
2015-12-03 12:02:34 +0300

    делаем запрос с сервака

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest: './upload/'});
-
+var request = require('request');
 var exec = require('child_process').exec;
 var child;
 
@@ -12,7 +12,13 @@ router.post('/', upload.single('image'), function(req, res, next) {
   var command = ["avconv -i /home/ubuntu/projects/JavaScript/video_to_audio/",req.file.path, " -vn -f wav /home/ubuntu/projects/JavaScript/video_to_audio/upload/audio/", req.file.filename,".wav"].join('');
   child = exec(command, function (error, stdout, stderr) {
     console.log(stdout);
-    res.json({url: ['http://159.122.249.236:3000/audio/',req.file.filename,'.wav'].join('')});
+    var murl = ['http://159.122.249.236:3000/audio/',req.file.filename,'.wav'].join('')
+    request('http://romancl.eu-gb.mybluemix.net/recognize?url='+murl, function (e) {
+      if (!error && response.statusCode == 200) {
+          res.json({url: murl});
+      }
+    })
+
     if (error !== null) {
       console.log('exec error: ' + error);
     }
