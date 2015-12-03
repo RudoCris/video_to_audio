8739725d94f8927813110a869c9672a8a9c72e6f
Rudolf <rud.manusachi@ru.ibm.com>
2015-12-03 04:37:57 +0300

    space before -vn

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var child;
 /* GET home page. */
 router.post('/', upload.single('image'), function(req, res, next) {
 
-  var command = ["avconv -i /home/ubuntu/projects/JavaScript/video_to_audio/",req.file.path, "-vn -f wav /home/ubuntu/projects/JavaScript/video_to_audio/upload/audio/", req.file.filename,".wav"].join('');
+  var command = ["avconv -i /home/ubuntu/projects/JavaScript/video_to_audio/",req.file.path, " -vn -f wav /home/ubuntu/projects/JavaScript/video_to_audio/upload/audio/", req.file.filename,".wav"].join('');
   child = exec(command, function (error, stdout, stderr) {
     console.log(stdout);
     if (error !== null) {
